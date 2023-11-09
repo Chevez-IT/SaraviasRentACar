969bed645adcdfeaf1974eac0d1ec407bae84550
erickfuentes.it@gmail.com <erickfuentes.it@gmail.com>
2023-11-09 03:15:29 -0600

    Implemetación de modelos

---
+++ src/main/java/sv/saraviasrenacar/www/empleados/controllers/EmpleadoVENController.java
@@ -0,0 +1,11 @@
+package sv.saraviasrenacar.www.empleados.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("EmpleadoVen")
+public class EmpleadoVENController {
+
+
+}
